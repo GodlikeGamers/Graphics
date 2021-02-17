2ee70064eafa77350573cf969c0c5039acb21652
GodlikeGamers <79181237+GodlikeGamers@users.noreply.github.com>
2021-02-17 23:44:22 +0300

    Innitial Commit

---
+++ script.js
@@ -0,0 +1,54 @@
+var objectf = document.getElementById('box_o'); 
+var heightt = objectf.clientHeight;
+
+function functionf(){
+        document.getElementById('box_o').style.height ="180px";
+        document.getElementById('btno').style.display ="none";
+        document.getElementById("destroyf").style.display ="block";
+        document.getElementById("destroyf").style.rotate ="45deg";
+}
+function destroyf(){
+    document.getElementById('box_o').style.height ="90px";
+    document.getElementById("destroyf").style.display ="none";
+    document.getElementById("destroyf").style.rotate ="-90deg";
+    document.getElementById('btno').style.display ="block";
+}
+
+function functions(){
+    document.getElementById('box_s').style.height ="180px";
+    document.getElementById('btns').style.display ="none";
+    document.getElementById("destroys").style.display ="block";
+    document.getElementById("destroys").style.rotate ="45deg";
+}
+function destroys(){
+    document.getElementById('box_s').style.height ="90px";
+    document.getElementById("destroys").style.display ="none";
+    document.getElementById("destroys").style.rotate ="-90deg";
+    document.getElementById('btns').style.display ="block";
+}
+
+function functionr(){
+    document.getElementById('box_r').style.height ="180px";
+    document.getElementById('btnr').style.display ="none";
+    document.getElementById("destroyr").style.display ="block";
+    document.getElementById("destroyr").style.rotate ="45deg";
+}
+function destroyr(){
+    document.getElementById('box_r').style.height ="90px";
+    document.getElementById("destroyr").style.display ="none";
+    document.getElementById("destroyr").style.rotate ="-90deg";
+    document.getElementById('btnr').style.display ="block";
+}
+
+function functionh(){
+    document.getElementById('box_h').style.height ="180px";
+    document.getElementById('btnh').style.display ="none";
+    document.getElementById("destroyh").style.display ="block";
+    document.getElementById("destroyh").style.rotate ="45deg";
+}
+function destroyh(){
+    document.getElementById('box_h').style.height ="90px";
+    document.getElementById("destroyh").style.display ="none";
+    document.getElementById("destroyh").style.rotate ="-90deg";
+    document.getElementById('btnh').style.display ="block";
+}
